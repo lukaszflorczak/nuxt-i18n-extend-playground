139533707ceb419db4ed95de6ae32e8e731d5f88
lukaszflorczak <1818979+lukaszflorczak@users.noreply.github.com>
2023-04-18 08:34:56 +0200

    3 files modified, 1 deleted

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,4 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/i18n'],
-  i18n: {
-    locales: [
-      {
-        code: 'en',
-        flag: 'us',
-        iso: 'en-US',
-        file: 'en.json',
-        name: 'English',
-      },
-      { code: 'pl', flag: 'pl', iso: 'pl-PL', file: 'pl.json', name: 'Polski' },
-    ],
-    defaultLocale: 'en',
-    strategy: 'prefix_except_default',
-    langDir: 'locales',
-    detectBrowserLanguage: {
-      useCookie: true,
-      // cookieKey: 'locale',
-      // fallbackLocale: 'en',
-      alwaysRedirect: true,
-      redirectOn: 'root',
-    },
-  },
+  extends: ['github:lukaszflorczak/i18n-layer-playground'],
 });
